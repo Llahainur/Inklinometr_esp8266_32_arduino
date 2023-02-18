81cd0aca58a5c200deae1aed9db7b63aa932b77e
Llahainur <94802067+Llahainur@users.noreply.github.com>
2023-02-18 12:49:15 +0300

    notW

--- src/server_html.h
+++ src/server_html.h
@@ -1,7 +1,6 @@
 // #include <ESP8266WebServer.h>
 // #include <ESP8266HTTPClient.h>
-
-#include <ESP8266WebServer.h>
+#include <ESP32WebServer.h>
 
 
 String SendHTML_onCalibration(float xA,float yA,float zA, float xG,float yG,float zG);
